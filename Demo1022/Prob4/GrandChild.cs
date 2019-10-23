2602f84344f918598af976153c4ccdfc537daf34
Bill Baker <bill@baker88.com>
2019-10-22 17:00:29 -0700

    10/22 Demos from PA 3

---
+++ Demo1022/Prob4/GrandChild.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prob4
+{
+    class GrandChild : Child
+    {
+        // Demonstrate overriding a base class method by a child
+        public override void SomeMethod()
+        {
+            Console.WriteLine("GrandChild's SomeMethod");
+        }
+
+        // Demonstrate a child method calling the parent's method
+        public override void Method2()
+        {
+            base.Method2();
+            Console.WriteLine("GrandChild.Method2");
+        }
+
+        public void SpecializedMethod()
+        {
+            Console.WriteLine("SpecializedMethod");
+        }
+    }
+}
